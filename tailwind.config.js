52276f1d33af517347cc628d9b2c6a0e5a8aaeff
chemvic <chemvic79@gmail.com>
2024-01-15 23:25:09 +0200

    сверстал страничку на Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  // darkMode: 'class',
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        'layout': 'auto 1fr auto',
+      },
+      colors: {
+        'logo-main': '#9b0404',
+      },
+      fontFamily:{
+        main: ['Montserrat'],
+      }
+    },
+  },
+  plugins: [],
+}
+
